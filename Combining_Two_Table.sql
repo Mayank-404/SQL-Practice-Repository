1d4e2fdcfa4edd1b0c5545b0b7a70d765b6e7dc4
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2025-08-13 19:34:45 +0530

    Update Combining_Two_Table.sql

--- Combining_Two_Table.sql
+++ Combining_Two_Table.sql
@@ -1,42 +1,26 @@
 /*
-Table: Person
+=============================================================================
+ Script:    Combining_Two_Table.sql
+ Purpose:   Report the first name, last name, city, and state of each person
+            from the Person table, including address information if available.
+=============================================================================
 
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| personId    | int     |
-| lastName    | varchar |
-| firstName   | varchar |
-+-------------+---------+
-personId is the primary key (column with unique values) for this table.
-This table contains information about the ID of some persons and their first and last names.
+-- Table: Person
+-- Columns: personId (PK), lastName, firstName
 
- 
+-- Table: Address
+-- Columns: addressId (PK), personId (FK), city, state
 
-Table: Address
-
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| addressId   | int     |
-| personId    | int     |
-| city        | varchar |
-| state       | varchar |
-+-------------+---------+
-addressId is the primary key (column with unique values) for this table.
-Each row of this table contains information about the city and state of one person with ID = PersonId.
-
-Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
-
-Return the result table in any order.
+-- Requirement:
+-- For each person, show their first name, last name, city, and state.
+-- If an address does not exist for a person, return NULL for city and state.
 */
-SELECT 
-    
-    p.lastname, 
-    p.firstname,
-    a.city, 
+
+SELECT
+    p.firstName,
+    p.lastName,
+    a.city,
     a.state
-FROM 
-    Person p
-LEFT JOIN 
-    Address a ON p.personId = a.personId; --this took 255ms 
+FROM
+    Person AS p
+    LEFT JOIN Address AS a ON p.personId = a.personId
