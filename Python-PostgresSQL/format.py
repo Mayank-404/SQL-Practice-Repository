04ecd51b106f8782ba51e9a7a0d16b63a58485fc
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2025-08-23 22:13:13 +0530

    Create format.py

---
+++ Python-PostgresSQL/format.py
@@ -0,0 +1,20 @@
+import psycopg2
+db= #database name
+use= # userID
+pas= # Admin password
+host = # Host
+port= # Post
+try:
+    connection = psycopg2.connect(database=db, user=use, password=pas, host=host, port=port)
+    cur = connection.cursor()
+   
+    query = f'Select From Where;'
+   
+    cur.execute(query)
+    connection.commit()
+    cur.close()
+except Exception as e:
+        # Handle the exception
+    print("An error occurred:", e)
+finally:
+    connection.close()
