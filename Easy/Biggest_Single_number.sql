a18b3125b172950f922491fe8589d3d4b1e182da
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2025-09-04 20:01:30 +0530

    Create Biggest_Single_number.sql

---
+++ Easy/Biggest_Single_number.sql
@@ -0,0 +1,25 @@
+/*
+===========================================================================================================
+Purpose: Find the largest single number. If there is no single number, report null.
+Link: https://leetcode.com/problems/biggest-single-number/description/
+===========================================================================================================
+-- Table   
+
+MyNumbers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+Each row of this table contains an integer.
+*/
+SELECT MAX(num) as num
+FROM Mynumbers
+WHERE num IN (
+    SELECT num
+    FROM Mynumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+)
