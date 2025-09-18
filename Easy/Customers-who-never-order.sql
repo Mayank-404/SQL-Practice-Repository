84dccb08c534601d2b4b82d2e5b1e228cee38eff
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2025-09-18 20:43:31 +0530

    Update Customers-who-never-order.sql

--- Easy/Customers-who-never-order.sql
+++ Easy/Customers-who-never-order.sql
@@ -10,7 +10,7 @@ Link:    https://leetcode.com/problems/customers-who-never-order/description/
       id		        (PK) ( representing a unique identifier for each order)
       customerId   (Foreign key)( referencing the id from the Customers table, indicating which customer placed the order.)
 */
--- Write your PostgreSQL query statement below
+
 select c.name as Customers 
 from Customers  c
  left join Orders o
